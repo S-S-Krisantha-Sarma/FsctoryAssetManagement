d75c0376c1a7f03912167c73d22521dedb1f8ace
S K Sharma <92776489+WebDev-SKSharma@users.noreply.github.com>
2025-07-17 20:52:58 +0530

    Add Tailwind CSS and refactor layout components
    - Updated `App.razor` to include Tailwind CSS and Font Awesome stylesheets.
    - Simplified `MainLayout.razor` by removing static elements for a more dynamic layout.
    - Introduced a conic-gradient background in `MainLayout.razor.css`.
    - Refactored `NavMenu.razor` to use a dynamic list of navigation links.
    - Removed duplicate `@page` directive in `Home.razor`.
    - Added `UserSecretsId` in `FactoryAssetManagement.csproj` for sensitive data management.
    - Created `Login.razor` for a new login page with Tailwind and Font Awesome integration.
    - Introduced `NavMenu.razor.cs` to define a `NavLink` class for better navigation structure.
    - Added `Reroute.razor` for redirecting from the root URL to `/Home`.
    - Created `input.css` for Tailwind CSS directives.
    - Added `tailwind.config.js` for Tailwind CSS configuration.
    - Introduced `tailwind.css` for default Tailwind styles.

---
+++ FactoryAssetManagement/FactoryAssetManagement/FactoryAssetManagement/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,23 @@
+namespace FactoryAssetManagement.Components.Layout
+{
+    public partial class NavMenu
+    {
+
+        public List<NavLink> NavLinks { get; set; } = new List<NavLink>()
+        {
+            new() { Name = "Home", Url = "/Home", Class = "bi bi-house-door-fill-nav-menu" },
+            new() { Name = "Counter", Url = "/counter", Class = "bi bi-plus-square-fill-nav-menu" },
+            new() { Name = "Weather", Url = "/weather", Class = "bi bi-list-nested-nav-menu" },
+            new() { Name = "Login", Url = "/login", Class = "fa-solid fa-key pe-3 text-xl" },
+        };
+
+
+    }
+
+    public class NavLink
+    {
+        public string Name { get; set; } = "";
+        public string Url { get; set; } = "";
+        public string Class { get; set; } = "";
+    }
+}
